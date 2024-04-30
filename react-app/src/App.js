0e27e1f12596d19e63cb411eb30b1c59cbf97355
tejaschapke18 <tejas.chapke@successive.tech>
2024-04-30 14:17:59 +0530

    changes

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Day1Component } from "./Day1";
 import { Day2Component } from "./Day2";
 import { Day3Component } from "./Day3";
+import { Day4Component } from "./Day4";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Day1Component />
       <Day2Component />
       <Day3Component />
+      <Day4Component />
     </div>
   );
 };
